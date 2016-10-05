89ad6a5a88e0930a31dea170fddc45375056f391
Jacques Laufer <jhlaufer@gmail.com>
2016-10-04 23:02:40 -0700

    Initial commit to GitHub

--- ADP_ACP CalcTests/ADP_ACP_CalcTests.swift
+++ ADP_ACP CalcTests/ADP_ACP_CalcTests.swift
@@ -28,7 +28,7 @@ class ADP_ACP_CalcTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
